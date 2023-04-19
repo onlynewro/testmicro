fdfe5704e9ea7cfdfda72b5e8afdc5b37255fe36
onlynewro <onlynewro@gmail.com>
2023-04-19 15:32:38 +0900

    biz 구현

---
+++ biz/test.go
@@ -0,0 +1,8 @@
+package main
+
+// GetHello returns a greeting message from the test.so shared library.
+func GetHello() string {
+	return "Hello from test.so!"
+}
+
+func main() {}
--- lib/lib.go
+++ lib/lib.go
@@ -1,11 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"C"
+	"net/http"
+	"path/filepath"
+	"plugin"
+)
 
 type HelloHandler struct{}
 
 func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
+	libraryPath, _ := filepath.Abs("../biz/test.so")
+	plug, err := plugin.Open(libraryPath)
+	if err != nil {
+		http.Error(w, "Error loading plugin", http.StatusInternalServerError)
+		return
+	}
+
+	gethello, err := plug.Lookup("GetHello")
+	if err != nil {
+		http.Error(w, "Error looking up function", http.StatusInternalServerError)
+		return
+	}
+
+	gethelloFunc, ok := gethello.(func() string)
+	if !ok {
+		http.Error(w, "Error asserting function", http.StatusInternalServerError)
+		return
+	}
+
+	message := gethelloFunc()
+	w.Write([]byte(message))
 }
 
 var Handler HelloHandler
